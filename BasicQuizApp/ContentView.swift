d7eb9ae3de37fff024752338dec74a595e640c87
Can Alpay <can.alpay@gmail.com>
2023-07-18 10:33:34 +0300

    created a questions array

--- BasicQuizApp/ContentView.swift
+++ BasicQuizApp/ContentView.swift
@@ -14,6 +14,12 @@ struct Question {
     let correctAnswerIndex: Int
 }
 
+let questions: [Question] = [
+    Question(text: "What is the capital of France?", answers: ["Paris", "London", "Berlin"], correctAnswerIndex: 0),
+    Question(text: "What is the capital of Germany?", answers: ["Berlin", "Paris", "Rome"], correctAnswerIndex: 0),
+    Question(text: "What is the capital of Italy?", answers: ["Rome", "Madrid", "Athens"], correctAnswerIndex: 0)
+]
+
 struct ContentView: View {
     var body: some View {
         VStack {
