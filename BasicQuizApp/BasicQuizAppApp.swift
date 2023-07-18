c76725b1893908f2e54f8b83d8d2f9e22f881614
Can Alpay <can.alpay@gmail.com>
2023-07-18 10:32:11 +0300

    Initial Commit

---
+++ BasicQuizApp/BasicQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicQuizAppApp.swift
+//  BasicQuizApp
+//
+//  Created by Can Alpay on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
